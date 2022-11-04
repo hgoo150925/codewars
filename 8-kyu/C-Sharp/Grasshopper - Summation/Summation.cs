297d79e7661488fcaa1586095ad7662a7f534b5d
hgo150925 <hugo.orlando2030@gmail.com>
2022-11-03 23:22:05 -0300

    Summation

---
+++ 8-kyu/C-Sharp/Grasshopper - Summation/Summation.cs
@@ -0,0 +1,19 @@
+using System;
+
+public static class Kata
+{
+    // Summation
+    // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+    // For example: summation(2) -> 3 -> 1 + 2
+    //summation(8) -> 36
+    // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+    public static int summation(int num)
+    {
+        int sum = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
