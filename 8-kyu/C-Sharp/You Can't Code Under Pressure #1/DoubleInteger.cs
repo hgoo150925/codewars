fdad9d0f48c074ea559b0d62a10b963dfe1133ba
hgo150925 <hugo.orlando2030@gmail.com>
2022-11-11 00:10:49 -0300

    You Can't Code Under Pressure #1

---
+++ 8-kyu/C-Sharp/You Can't Code Under Pressure #1/DoubleInteger.cs
@@ -0,0 +1,9 @@
+using System;
+
+public static class Kata
+{
+    // You Can't Code Under Pressure #1
+    // Code as fast as you can! You need to double the integer and return it.
+    // https://www.codewars.com/kata/53ee5429ba190077850011d4/train/csharp
+    public static int DoubleInteger(int n) => n * 2;
+}
